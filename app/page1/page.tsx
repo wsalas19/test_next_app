aa84929ea680921bcb578c9c5733a466d03dd745
wsalas19 <willbullet52@gmail.com>
2023-09-02 14:45:04 -0500

    some changes

---
+++ app/page1/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page1() {
+	return (
+		<div className='flex w-full h-screen justify-center items-center '>
+			<h1 className='text-3xl font-bold text-white'>Page 1</h1>
+		</div>
+	);
+}
+
+export default page1;
